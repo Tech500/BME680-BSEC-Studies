f2a9dfcfa56554da83d3e1b8b8809b05164ee2d0
William Lucid <11734452+Tech500@users.noreply.github.com>
2021-03-12 10:37:06 -0500

    Add files via upload

---
+++ index3.h
@@ -0,0 +1,34 @@
+//index3.h
+const char HTML4[] PROGMEM = R"====(
+<!DOCTYPE HTML>   
+<html>
+
+<head>
+    <title>BME680 BSEC Graphs</title>
+</head>
+
+<body>
+  <br>
+	<h2>BME680 Gas Study</h2>
+	<br>
+  <frameset rows="30%,70%" cols="33%,34%">
+  </frameset>
+  Your Iframe goe here... Field 1
+  Your Iframe goe here... Field 2
+  <br><br>
+  <frameset rows="30%,70%" cols="33%,34%">
+  Your Iframe goe here... Field 3
+  Your Iframe goe here... Field 4
+  </frameset>
+  <br><br>
+  <frameset rows="30%,70%" cols="33%,34%">
+  Your Iframe goe here... Field 5
+  Your Iframe goe here... Field 6
+  </frameset>
+  <br><br>
+  <h2><a href="%LINK%/BME680" >Home</a></h2>
+  <br>
+</body>
+
+</html>
+)====";
